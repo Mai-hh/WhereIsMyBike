92be969c39d96b0a39f15e2e67191b16148be868
Mai-hh <mohuaihaoxm@163.com>
2022-03-28 18:44:06 +0800

    init

---
+++ app/src/main/java/com/example/whereismybike/widget/WhereBikeAppWidgetProvider.kt
@@ -0,0 +1,20 @@
+package com.example.whereismybike.widget
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+
+class WhereBikeAppWidgetProvider : AppWidgetProvider() {
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        appWidgetIds.forEach { appWidgetId ->
+
+            
+        }
+
+    }
+}
